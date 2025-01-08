231c3d26ffd01ee6936ac11c1c78110e73a185ca
mavenskylab <mavenskylab@gmail.com>
2025-01-08 10:09:19 +0000

    patch: client side validation

--- hooks/use-form-state.ts
+++ hooks/use-form-state.ts
@@ -1,39 +1,70 @@
 'use client'
 
 import { FormState } from '@/types/form'
-import { useEffect, useState, useTransition } from 'react'
-import { useFormContext } from 'react-hook-form'
+import { isEqual } from '@react-hookz/deep-equal'
+import { useEffect, useRef, useState, useTransition } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 export type UseFormStateReturn<
-  T extends object,
+  S extends z.Schema,
+  T extends z.infer<S>,
   P extends unknown[],
-> = ReturnType<typeof useFormState<T, P>>
-export type Form<T extends object> = UseFormStateReturn<T, unknown[]>[0]
+> = ReturnType<typeof useFormState<S, T, P>>
+export type Form<S extends z.Schema> = UseFormStateReturn<
+  S,
+  z.infer<S>,
+  unknown[]
+>[0]
 
-export function useFormState<T extends object, P extends unknown[]>(
+export function useFormState<
+  S extends z.Schema,
+  T extends z.infer<S>,
+  P extends unknown[],
+>(
+  schema: S,
   action: (state: FormState<T>, ...payload: P) => Promise<FormState<T>>,
   initialState: FormState<T>,
 ) {
-  const form = useFormContext<T>()
-  const [state, formAction, isPending] = useActionState(action, initialState)
+  const [state, formAction, reset, isPending] = useActionState(
+    action,
+    initialState,
+  )
+  const form = useForm<T>({
+    mode: 'onTouched',
+    criteriaMode: 'all',
+    defaultValues: { ...initialState.data, ...state.data } as T,
+    //@ts-ignore
+    errors: state.errors,
+    resolver: async (data) => {
+      const { error } = await schema.safeParseAsync(data)
+      return (error ? { errors: error?.format() } : {}) as any
+    },
+  })
 
   useEffect(() => {
-    form.reset()
-    form.reset(state.data as T)
+    form.reset(undefined, { keepErrors: true })
+    form.reset(state.data as T, { keepErrors: true })
   }, [state])
+
+  const values = form.watch()
+
+  const { success: isValid } = schema.safeParse(values)
 
   return [
     {
       ...form,
       formState: {
         ...form.formState,
-        errors: state.errors,
+        errors: form.formState.errors as FormState<T>['errors'],
+        isValid,
         isPending,
         isSubmitSuccessful: state.success,
         message: state.message,
       },
     },
     formAction,
+    reset,
   ] as const
 }
 
@@ -41,8 +72,16 @@ function useActionState<T, P extends unknown[]>(
   action: (state: T, ...payload: P) => Promise<T>,
   initialState: T,
 ) {
-  const [state, setState] = useState(initialState)
+  const initialStateRef = useRef(initialState)
+  const [state, setState] = useState(() => initialStateRef.current)
   const [pending, startTransaction] = useTransition()
+
+  useEffect(() => {
+    if (!isEqual(initialStateRef.current, initialState)) {
+      initialStateRef.current = initialState
+      reset()
+    }
+  }, [initialState])
 
   async function formAction(...payload: P) {
     startTransaction(async () => {
@@ -51,5 +90,9 @@ function useActionState<T, P extends unknown[]>(
     })
   }
 
-  return [state, formAction, pending] as const
+  function reset() {
+    setState(initialStateRef.current)
+  }
+
+  return [state, formAction, reset, pending] as const
 }
